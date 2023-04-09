8b38fbbbff135d81472503fa67ee4a4f3432c65f
Donut3183 <github@doughnut.mailer.me>
2023-04-09 15:02:55 +0200

    nvim

--- nvim/.config/nvim/lua/custom/configs/overrides.lua
+++ nvim/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,61 +1,67 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"prettier",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
-    -- Latex stuff
-    "ltex-ls",
-  },
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
+		-- Latex stuff
+		"ltex-ls",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
-
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	git = {
+		enable = true,
+	},
+	view = {
+		adaptive_size = true,
+		side = "left",
+		width = 30,
+		hide_root_folder = true,
+		preserve_window_proportions = true,
+	},
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 return M
