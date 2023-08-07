2d0f9f00f45ce6bf3e5d762f1a5a7dc036da0f59
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-08-07 22:26:02 +0200

    hyprland config

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -2,7 +2,7 @@
 
 autorandr --load triple_home &
 sleep 1
-picom -b  --config ~/.config/picom/picom.conf &
+picom -b --config ~/.config/picom/picom.conf &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 nm-applet &
 /usr/bin/dunst &
@@ -15,26 +15,13 @@ spotify-launcher &
 thunderbird &
 mullvad-vpn &
 signal-desktop --start-in-tray &
-<<<<<<< HEAD
-solaar --window=hide &
-=======
 spotify-launcher &
 # solaar --window=hide &
->>>>>>> 9589f8ea (Untracked cache from prusa slicer, updated dwm-bar script, dwm autostart added a blanking script used with udev)
 
 sleep 1
 feh --bg-fill /home/pontus/Nextcloud/Pictures/Wallapaper/gruvbox_Fnatic_Elements_Earth_Wallpaper_2560x1440.jpg &
-<<<<<<< HEAD
 sleep 5
 if pgrep -x "dwm-bar" > /dev/null; then
 else
     dwm-bar &
 fi
-connect-xm3 & > /dev/null 2>&1
-=======
-# sleep 5
-# if pgrep -x "dwm-bar" > /dev/null; then
-# else
-#     dwm-bar &
-# fi
->>>>>>> 9589f8ea (Untracked cache from prusa slicer, updated dwm-bar script, dwm autostart added a blanking script used with udev)
