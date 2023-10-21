4ace6ba8cf99d8a8d869a42bce78496cb53cd4ec
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-10-21 22:48:51 +0200

    scripting

--- scripts/.config/scripts/start-matlab.sh
+++ scripts/.config/scripts/start-matlab.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/usr/local/MATLAB/R2023b/bin/matlab -desktop -r "edit('$1')"
+if [ -z "$1" ]; then
+  /usr/local/MATLAB/R2023b/bin/matlab -desktop
+else
+  /usr/local/MATLAB/R2023b/bin/matlab -desktop -r "edit('$1')"
+fi
