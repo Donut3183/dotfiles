bb845f0a8ce798bd75b05f60bb3709f69d81ed9a
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-11-25 14:15:13 +0100

    dwm autostart, autorandr, and dwm-bar restart configured

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-autorandr --load triple_home &
+autorandr --change &
 sleep 1
 picom -b --config ~/.config/picom/picom.conf &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
@@ -17,12 +17,15 @@ mullvad-vpn &
 signal-desktop --start-in-tray &
 spotify-launcher &
 discord &
+export XAUTHORITY=$(find /tmp -name 'xauth_*' -user pontus 2>/dev/null)
+ln -sf "$XAUTHORITY" /home/pontus/.Xauthority
 # solaar --window=hide &
 
 sleep 1
 feh --bg-fill /home/pontus/Nextcloud/Pictures/Wallapaper/gruvbox_Fnatic_Elements_Earth_Wallpaper_2560x1440.jpg &
 sleep 5
 if pgrep -x "dwm-bar" > /dev/null; then
+  pkill dwm-bar && dwm-bar &
 else
     dwm-bar &
 fi
