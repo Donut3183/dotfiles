59850d7c215725a56ddcab314288491fe246e5f6
Pontus Svensson <pontus@doughnut.anonaddy.com>
2023-11-26 16:07:51 +0100

    using other programs on pc

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -6,7 +6,7 @@ picom -b --config ~/.config/picom/picom.conf &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 nm-applet &
 /usr/bin/dunst &
-blueberry-tray &
+blueman-applet &
 pasystray &
 protonmail-bridge --no-window &
 nextcloud &
@@ -17,13 +17,14 @@ mullvad-vpn &
 signal-desktop --start-in-tray &
 spotify-launcher &
 discord &
+
 export XAUTHORITY=$(find /tmp -name 'xauth_*' -user pontus 2>/dev/null)
 ln -sf "$XAUTHORITY" /home/pontus/.Xauthority
 # solaar --window=hide &
 
 sleep 1
 feh --bg-fill /home/pontus/Nextcloud/Pictures/Wallapaper/gruvbox_Fnatic_Elements_Earth_Wallpaper_2560x1440.jpg &
-sleep 5
+# sleep 5
 if pgrep -x "dwm-bar" > /dev/null; then
   pkill dwm-bar && dwm-bar &
 else
