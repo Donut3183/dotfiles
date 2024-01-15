27dc5bafdd0cba62d3dbcff4d57574a2c43206f2
Pontus Svensson <pontus@doughnut.anonaddy.com>
2024-01-15 23:53:05 +0100

    start-matab directory changge

--- scripts/.config/scripts/start-matlab.sh
+++ scripts/.config/scripts/start-matlab.sh
@@ -2,7 +2,7 @@
 # export QT_QPA_PLATFORM=xcb
 export LD_PRELOAD="/usr/lib/libstdc++.so.6"
 if [ -z "$1" ]; then
-	lockdev-redirect /usr/local/MATLAB/R2023b/bin/matlab -desktop
+	lockdev-redirect /opt/matlab/R2023b/bin/matlab -desktop
 else
-	lockdev-redirect /usr/local/MATLAB/R2023b/bin/matlab -desktop -r "edit('$1')"
+	lockdev-redirect /opt/matlab/R2023b/bin/matlab -desktop -r "edit('$1')"
 fi
