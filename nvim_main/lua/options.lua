04ec75e1c72406d34f282df3acf8a54c3977f73f
Pontus Svensson <pontus.svensson@pontuss.com>
2025-01-20 21:26:47 +0100

    nvim

---
+++ nvim_main/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.o
+local opt = vim.opt
+o.cursorlineopt = 'both' -- to enable cursorline!
+opt.wrap = true
+opt.linebreak = true    -- Prevent breaking in the middle of words
+opt.breakindent = true  -- Indent wrapped lines for readability
