3f0defa3dbb7bb1b235246a2f0080da52ba1f33e
Pontus <github@doughnut.mailer.me>
2025-03-02 16:56:53 +0100

    Added servers to Mason for automatic install

--- nvim_main/lua/options.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-local o = vim.o
-local opt = vim.opt
-o.cursorlineopt = 'both' -- to enable cursorline!
-opt.wrap = true
-opt.linebreak = true    -- Prevent breaking in the middle of words
-opt.breakindent = true  -- Indent wrapped lines for readability
